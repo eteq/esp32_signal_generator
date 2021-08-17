7e9dd07a41913ccfa759bdf413a04f2caf7bee6b
Erik Tollerud <erik.tollerud@gmail.com>
2021-08-17 02:26:53 -0400

    signal generator working

--- main/main.c
+++ main/main.c
@@ -1,142 +1,138 @@
 #include <stdio.h>
-#include <math.h>
-#include <driver/adc.h>
-#include <driver/dac.h>
-#include <driver/gpio.h>
-#include <esp_timer.h>
+
+#include "driver/dac.h"
+#include "driver/touch_pad.h"
+#include "driver/sigmadelta.h"
+#include "esp_timer.h"
+#include "esp_system.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-//ADC1_CHANNEL_6 -> GPIO 34
 
-#define ADC_CHANNEL ADC1_CHANNEL_6
-#define ADC_ATTEN ADC_ATTEN_DB_11
-#define ADC_WIDTH ADC_WIDTH_BIT_12
+//DAC channel 1 is GPIO25
+//DAC channel 2 is GPIO26
 
-#define ARRAY_SIZE 4096
+#define TOUCH_PIN 7  //T7 = GPIO27 - on huzzah that's 6th pin from the battery connector
+#define TOUCHPAD_FILTER_TOUCH_PERIOD 10
+#define THRESH_SCALE 2 / 3
 
-#define GPIO_OUTPUT_PIN 21
+#define LED_GPIO GPIO_NUM_13
+
+#define NFREQS 6
+uint32_t FREQS[NFREQS] = {130, 260, 1250, 2500, 5000, 25000};
 
 
-int adc_raw_arr[ARRAY_SIZE];
+bool touching = false, increment = false;
+int touch_i = 0;
 
-void setup_adc1(void) {
-    adc1_config_width(ADC_WIDTH);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN);
-}
 
-void setup_gpio(void) {
-    gpio_config_t io_conf;
-
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = 1ULL << GPIO_OUTPUT_PIN;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    gpio_set_level(GPIO_OUTPUT_PIN, 0);
-}
-
-void setup_cw(void) {
+esp_err_t setup_cw(uint32_t freq) {
     dac_output_enable(DAC_CHANNEL_2);
 
     dac_cw_config_t cw_conf;
 
     cw_conf.en_ch = DAC_CHANNEL_2;
-    cw_conf.scale = DAC_CW_SCALE_2;
+    cw_conf.scale = DAC_CW_SCALE_1;
     cw_conf.phase = DAC_CW_PHASE_0;
-    cw_conf.freq = 2335;
-    cw_conf.offset = 25;
+    cw_conf.freq = freq;
+    cw_conf.offset = 0;
+
+    return dac_cw_generator_config(&cw_conf);
 
     if (dac_cw_generator_config(&cw_conf) == ESP_OK) {
-        dac_cw_generator_enable();
-        printf("CW startup success.\n");
+        printf("CW setup success.\n");
     } else {
-        printf("CW startup failed.\n");
+        printf("CW setup failed.\n");
     };
 }
 
 
-double fill_adc1_arr(void) {
-    // return value is dt per sample in microseconds
-
-    int64_t t1, t2;
-    t1 = esp_timer_get_time();
-    int ao2 = ARRAY_SIZE / 2;
-
-    for (int i=0; i < ARRAY_SIZE; i++ ) {
-        adc_raw_arr[i] = adc1_get_raw((adc1_channel_t)ADC_CHANNEL);
+static void touch_isr(void *arg) {
+    if ((touch_pad_get_status() > 0) & ! touching) {
+        increment = true;
+        touching = true;
     }
-    t2 = esp_timer_get_time();
-    return (double)(t2 - t1)/(double)ARRAY_SIZE;
+    touch_pad_clear_status();
 }
 
-void cw_main(void)
-{
-    double dt;
+void setup_touch(void) {
+    uint16_t touch_value;
 
-    printf("Hello world!\n");
+    touch_pad_init();
+    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
+    touch_pad_config(TOUCH_PIN, 0);
+    touch_pad_filter_start(TOUCHPAD_FILTER_TOUCH_PERIOD);
 
-    setup_adc1();
-    adc1_get_raw((adc1_channel_t)ADC_CHANNEL);  // Might not be necessary
+    // threshold
+    touch_pad_read_filtered(TOUCH_PIN, &touch_value);
+    touch_pad_set_thresh(TOUCH_PIN, touch_value * THRESH_SCALE);
 
-    setup_cw();
+    touch_pad_isr_register(touch_isr, NULL);
 
+    touch_pad_intr_enable();
+}
+
+esp_err_t setup_led(void) {
+    sigmadelta_config_t sigmadelta_cfg = {
+        .channel = SIGMADELTA_CHANNEL_0,
+        .sigmadelta_prescale = 80,
+        .sigmadelta_duty = 127,
+        .sigmadelta_gpio = LED_GPIO,
+    };
+    return sigmadelta_config(&sigmadelta_cfg);
+}
+
+
+void app_main(void) {
+    uint16_t touch_value, thresh_value;
+
+    if (setup_led() == ESP_OK) {
+        printf("LED setup success.\n");
+    } else {
+        printf("LED setup failed.\n");
+    };
+
+    setup_touch();
+    touch_pad_get_thresh(TOUCH_PIN, &thresh_value);
+    printf("Touch value threshold at startup: %d\n", thresh_value);
+
+    if (setup_cw(FREQS[0]) == ESP_OK) {
+        printf("CW setup success.\n");
+    } else {
+        printf("CW setup failed.\n");
+    };
+
+    dac_cw_generator_enable();
+    sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, -128);  // Initialize to off once things are actually going
 
     while (1) {
-        printf("Voltage raw: %d\n", adc1_get_raw((adc1_channel_t)ADC_CHANNEL));
-        printf("Time since startup: %lld\n", esp_timer_get_time());
+        if (increment) {
+            touch_i++;
 
-        printf("Reading ADC\n");
-        dt = fill_adc1_arr();
-        printf("Data:\n");
+            if (setup_cw(FREQS[touch_i % NFREQS]) == ESP_OK) {
+               printf("Set to new frequency %d\n", FREQS[touch_i % NFREQS]);
+            } else {
+               printf("Failed to set to new frequency %d\n", FREQS[touch_i % NFREQS]);
+            };
 
-        for (int i=0; i < (ARRAY_SIZE-1); i++ ) {
-            printf("%d,", adc_raw_arr[i]);
+            double touchi_frac = (touch_i % NFREQS) / (NFREQS-1.);
+            sigmadelta_set_duty(SIGMADELTA_CHANNEL_0, 255 * (touchi_frac*touchi_frac) - 128);
+
+            increment = false;
         }
-        printf("%d\ndt per sample=%f\n", adc_raw_arr[ARRAY_SIZE-1], dt);
-
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
-}
-
-void rc_main(void)
-{
-    double dt;
-
-    printf("Hello world!\n");
-
-    setup_adc1();
-    adc1_get_raw((adc1_channel_t)ADC_CHANNEL);  // Might not be necessary
 
 
-    while (1) {
-        printf("Voltage raw: %d\n", adc1_get_raw((adc1_channel_t)ADC_CHANNEL));
-        printf("Time since startup: %lld\n", esp_timer_get_time());
-
-        printf("Charging cap for 2 sec\n");
-        gpio_set_level(GPIO_OUTPUT_PIN, 1);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-
-        printf("Reading ADC\n");
-        gpio_set_level(GPIO_OUTPUT_PIN, 0);
-        dt = fill_adc1_arr();
-        printf("Data:\n");
-
-        for (int i=0; i < (ARRAY_SIZE-1); i++ ) {
-            printf("%d,", adc_raw_arr[i]);
+        if (touching) {
+            // allow for some debounce if need be
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+            touch_pad_read_filtered(TOUCH_PIN, &touch_value);
+            if (touch_value > thresh_value) {
+                touching = false;
+            }
         }
-        printf("%d\ndt per sample=%f\n", adc_raw_arr[ARRAY_SIZE-1], dt);
 
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
-}
-
-
-
-void app_main(void)
-{
-    cw_main();
 }
